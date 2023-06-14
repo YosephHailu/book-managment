bfeec310ddbf6c97293381ffe37cb99ceca83492
YosephHailu <hailuyoseph9@gmail.com>
2023-06-14 16:18:56 +0300

    feat:create store book form request and add validations

---
+++ app/Http/Requests/StoreBookRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|max:255',
+            'author' => 'required',
+            'publication_year' => 'required|integer',
+            'genre' => 'required',
+            'cover_image' => 'required|image',
+            'summery' => 'nullable'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ], 422));
+    }
+}
