b1f793edfa8e3b7b708ecca56f343206135f8250
YosephHailu <hailuyoseph9@gmail.com>
2023-06-14 16:20:47 +0300

    feat:create migration for book model

---
+++ database/migrations/2023_06_13_150256_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->integer('publication_year')->min(0);
+            $table->string('genre');
+            $table->longText('summery')->nullable();
+            $table->string('cover_image')->default('cover_image_placeholder.png');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
